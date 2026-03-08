0a735058d66398ca7043d319e7150c4df9ae369d
VishalY9464 <yaddavvishal9464@gmail.com>
2026-03-08 12:24:31 +0530

    [Vishal Yadav]:UC13 Read and Write AddressBook using File IO

--- src/main/java/com/addressbook/model/Person.java
+++ src/main/java/com/addressbook/model/Person.java
@@ -93,7 +93,7 @@ public class Person {
 	@Override
 	public boolean equals(Object obj) {
 
-		if (this == obj)
+		if (this == obj)    
 			return true;
 
 		if (obj == null || getClass() != obj.getClass())
