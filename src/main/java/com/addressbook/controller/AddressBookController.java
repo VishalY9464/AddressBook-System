3db9a7942f901d55d2b21f13b0b6e47528855a37
VishalY9464 <yaddavvishal9464@gmail.com>
2026-03-09 10:29:09 +0530

    [Vishal Yadav]: UC23 Update contact using Rest API

--- src/main/java/com/addressbook/controller/AddressBookController.java
+++ src/main/java/com/addressbook/controller/AddressBookController.java
@@ -3,12 +3,18 @@ package com.addressbook.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.addressbook.model.Person;
 import com.addressbook.service.AddressBook;
 
 @RestController
+@RequestMapping("/contacts")
 public class AddressBookController {
 
     private AddressBook addressBook = new AddressBook();
@@ -26,9 +32,39 @@ public class AddressBookController {
     }
 
     // Get all contacts
-    @GetMapping("/contacts")
+    @GetMapping
     public List<Person> getContacts() {
 
         return addressBook.getContacts();
+    }
+    
+    // Add new contact
+    @PostMapping
+    public String addContact(@RequestBody Person person) {
+
+        addressBook.addContact(person);
+
+        return "Contact added successfully";
+    }
+    
+ // Update contact
+    @PutMapping("/{name}")
+    public String updateContact(@PathVariable String name, @RequestBody Person updatedPerson) {
+
+        List<Person> contacts = addressBook.getContacts();
+
+        for (Person person : contacts) {
+
+            if (person.getFirstName().equalsIgnoreCase(name)) {
+
+                person.setAddress(updatedPerson.getAddress());
+                person.setCity(updatedPerson.getCity());
+                person.setState(updatedPerson.getState());
+
+                return "Contact updated successfully";
+            }
+        }
+
+        return "Contact not found";
     }
 }
